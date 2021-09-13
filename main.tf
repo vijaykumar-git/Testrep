1e9f25c8f8f0aea00aa78390b4d80792a4b76e51
vijaykumar-git <90051090+vijaykumar-git@users.noreply.github.com>
2021-09-13 16:19:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "shahid-test"
+    organization = "vijayorg"
 
     workspaces {
-      name = "shahid-actions-demo"
+      name = "gh-actions-demo"
     }
   }
 }
